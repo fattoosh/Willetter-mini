cc2aba59c12408ea877555344f9b2181b334aaa7
Omar Fityan <omarfityan@yahoo.com>
2018-10-03 21:47:59 +0300

    Initial Commit

---
+++ Willetter-mini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Willetter-mini
+//
+//  Created by Omar Fityan on 10/3/18.
+//  Copyright © 2018 Omar Fityan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
